655163ee79609351c004740f1b78ee1792dbc115
miguel angel serrano <aristizabalmiguelangel50@gmail.com>
2024-10-20 15:32:41 -0500

    Se realiza actualizacion con el breze

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\ProveedorController;
-use App\Http\Controllers\OrdenCompraController;
 
-// Agrupamos las rutas que requieren autenticación con el middleware 'auth'
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('productos', ProductoController::class);
-    Route::resource('proveedores', ProveedorController::class);
-    Route::resource('ordenes-de-compra', OrdenCompraController::class);
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
